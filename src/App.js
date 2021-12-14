4f538ab843fd25aeba8a58599382ee71e337ba19
Bahtiyorjon Arslonbekov <bahtiyorjonq777@gmail.com>
2021-12-14 23:16:59 +0100

    navbar content created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import styled from "styled-components";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <AppWrap>
+      <NavBar>
+        <NavCont>
+          <Icon>
+            <img src="" alt="" />
+          </Icon>
+          <Content>
+            <li>Home</li>
+            <li>Add Member</li>
+            <li>Log Out</li>
+          </Content>
+        </NavCont>
+      </NavBar>
+    </AppWrap>
+  );
 }
 
+const AppWrap = styled.div`
+  text-align: center;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  width: 100vw;
+  min-height: 100vh;
+  background: #ac6464;
+`;
+
+const NavBar = styled.nav`
+  height: 65px;
+  background: #ca9e9e;
+`;
+
+const NavCont = styled.div``;
+
+const Icon = styled.div``;
+
+const Content = styled.ul``;
 export default App;
