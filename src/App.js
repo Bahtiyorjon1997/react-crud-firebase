4e61212ada53317ee63a03162aae50bcda2f7081
Bahtiyorjon Arslonbekov <bahtiyorjonq777@gmail.com>
2021-12-15 21:58:09 +0100

    nav layout built

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import logo from "./images/logo.svg";
 
 function App() {
   return (
@@ -6,7 +7,7 @@ function App() {
       <NavBar>
         <NavCont>
           <Icon>
-            <img src="" alt="" />
+            <img src={logo} alt="logo" />
           </Icon>
           <Content>
             <li>
@@ -34,16 +35,34 @@ const AppWrap = styled.div`
   width: 100vw;
   min-height: 100vh;
   background: #ac6464;
+  display: flex;
+  flex-direction: column;
 `;
 
 const NavBar = styled.nav`
   height: 65px;
   background: #ca9e9e;
+  display: wrap;
+  align-items: center;
+
+  right: 5rem;
+  left: 5rem;
 `;
 
-const NavCont = styled.div``;
+const NavCont = styled.div`
+  margin: 0 5rem;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid black;
+`;
 
-const Icon = styled.div``;
+const Icon = styled.div`
+  box-sizing: border-box;
+
+  img {
+    max-height: 45px;
+  }
+`;
 
 const Content = styled.ul``;
 
